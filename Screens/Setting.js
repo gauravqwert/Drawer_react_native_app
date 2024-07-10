f50c034952f3146a2d735d55d252234cad31ad96
unknown <patelgaurav282000@gmail.com>
2024-07-10 10:17:08 +0530

    Drawer app in react native

---
+++ Screens/Setting.js
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const SettingScreen = () => {
+  return (
+    <View style={{flex:1,alignItems:"center",justifyContent:"center"}}>
+      <Text>Hello , This is settings Screen</Text>
+    </View>
+  )
+}
+
+export default SettingScreen;
